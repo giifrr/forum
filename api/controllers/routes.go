d92f66febee9d8d721a8a10af83bd36684d9811b
Gifar Al Anshar <gifar.anshar12@gmail.com>
2023-07-23 06:01:52 +0700

    feat: create get all users

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -1,25 +1,14 @@
 package controllers
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
 func (s *Server) InitializeRoutes() {
 	v1 := s.Router.Group("/api/v1")
 	{
-		// Root path
-		v1.GET("/", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "Hello, world!",
-			})
-		})
 
 		// login route
 		v1.POST("/login", s.Login)
 
 		// User routes
 		v1.POST("/users", s.CreateUser)
+		v1.GET("/users", s.GetUsers)
 	}
 }
